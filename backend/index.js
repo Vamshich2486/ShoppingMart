61d21aae099aaf58ecd9704103034637175bca4c
Vamshich2486 <vamshi2486ch@gmail.com>
2025-02-27 09:33:44 +0530

    orders management done

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,8 @@ import connectDB from "./config/db.js"
 import userRoutes from "./routes/userRoutes.js"
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
-import uploadRoutes from "./routes/uploadRoutes.js"
+import uploadRoutes from "./routes/uploadRoutes.js";
+import orderRoutes from "./routes/orderRoutes.js"
 
 dotenv.config();
 const port=process.env.PORT || 5000;
@@ -23,7 +24,12 @@ app.use(cookieParser());
 app.use("/api/users",userRoutes);
 app.use("/api/category" ,categoryRoutes);
 app.use("/api/products",productRoutes);
+app.use("/api/orders", orderRoutes);
 app.use("/api/upload", uploadRoutes);
+
+app.get("/api/config/paypal", (req,res)=>{
+    res.send({clientId: process.env.PAYPAL_CLIENT_ID});
+})
 
 const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname + "/uploads")));
