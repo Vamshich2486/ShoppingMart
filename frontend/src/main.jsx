a298fc274401f5daa7bb81ab14f0880f8e589f70
Vamshich2486 <vamshi2486ch@gmail.com>
2025-02-28 16:10:31 +0530

    done with orders management and integration with paypal payments

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,71 +1,83 @@
 //import { StrictMode } from 'react'
-import React from 'react';
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
 import "./index.css";
-import { Route,RouterProvider,createRoutesFromElements } from 'react-router';
-import { createBrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import store from './redux/store.js';
+import { Route, RouterProvider, createRoutesFromElements } from "react-router";
+import { createBrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./redux/store.js";
 
 //Private Route
-import PrivateRoutes from './components/PrivateRoutes.jsx';
+import PrivateRoutes from "./components/PrivateRoutes.jsx";
 
 //Auth
-import Login from './pages/Auth/Login.jsx';
-import Register from './pages/Auth/Register.jsx';
-import Profile from './pages/User/Profile.jsx';
+import Login from "./pages/Auth/Login.jsx";
+import Register from "./pages/Auth/Register.jsx";
+import Profile from "./pages/User/Profile.jsx";
 
-import AdminRoute from './pages/Admin/AdminRoute.jsx';
-import UserList from './pages/Admin/UserList.jsx';
-import CategoryList from './pages/Admin/CategoryList.jsx';
-import ProductList from './pages/Admin/ProductList.jsx';
-import AllProducts from './pages/Admin/AllProducts.jsx';
-import ProductUpdate from './pages/Admin/ProductUpdate.jsx';
-import Home from './pages/Home.jsx';
+import AdminRoute from "./pages/Admin/AdminRoute.jsx";
+import UserList from "./pages/Admin/UserList.jsx";
+import CategoryList from "./pages/Admin/CategoryList.jsx";
+import ProductList from "./pages/Admin/ProductList.jsx";
+import AllProducts from "./pages/Admin/AllProducts.jsx";
+import ProductUpdate from "./pages/Admin/ProductUpdate.jsx";
+import Home from "./pages/Home.jsx";
 
-import Favorites from './pages/Products/Favorites.jsx';
-import ProductDetails from './pages/Products/ProductDetails.jsx';
+import Favorites from "./pages/Products/Favorites.jsx";
+import ProductDetails from "./pages/Products/ProductDetails.jsx";
 
-import Cart from './pages/Cart.jsx';
-import Shop from './pages/Shop.jsx';
+import Cart from "./pages/Cart.jsx";
+import Shop from "./pages/Shop.jsx";
+import Shipping from "./pages/Orders/Shipping.jsx";
+import PlaceOrder from "./pages/Orders/PlaceOrder.jsx";
 
-const router=createBrowserRouter(
+import {PayPalScriptProvider} from "@paypal/react-paypal-js"
+import Order from "./pages/Orders/Order.jsx";
+import UserOrder from "./pages/User/UserOrder.jsx";
+import OrderList from "./pages/Admin/OrderList.jsx";
+import AdminDashboard from "./pages/Admin/AdminDashboard.jsx";
+
+const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<App/>}>
+    <Route path="/" element={<App />}>
       <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} /> 
+      <Route path="/register" element={<Register />} />
       <Route index={true} path="/" element={<Home />} />
-      <Route path='/favorite' element={<Favorites />} />
-      <Route path="/product/:id" element={<ProductDetails />} /> 
+      <Route path="/favorite" element={<Favorites />} />
+      <Route path="/product/:id" element={<ProductDetails />} />
       <Route path="/cart" element={<Cart />} />
       <Route path="/shop" element={<Shop />} />
+      <Route path="/user-orders" element={<UserOrder />} />
 
-      <Route path="" element={<PrivateRoutes />} >
-        <Route path='/profile' element={<Profile />} />
+      <Route path="" element={<PrivateRoutes />}>
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/shipping" element={<Shipping />} />
+        <Route path="/placeorder" element={<PlaceOrder />} />
+        <Route path="/order/:id" element={<Order />} />
       </Route>
 
       {/* admin route */}
       <Route path="/admin" element={<AdminRoute />}>
-          <Route path="userlist" element={<UserList />} />
-          <Route path="categorylist" element={<CategoryList />} /> 
-          <Route path='productlist' element={<ProductList />} />
-          <Route path="allproductslist" element={<AllProducts />} />
-          <Route path="product/update/:_id" element={<ProductUpdate />} />
+        <Route path="userlist" element={<UserList />} />
+        <Route path="categorylist" element={<CategoryList />} />
+        <Route path="orderlist" element={<OrderList />} />
+        <Route path="dashboard" element={<AdminDashboard />} />
+        <Route path="productlist" element={<ProductList />} />
+        <Route path="allproductslist" element={<AllProducts />} />
+        <Route path="product/update/:_id" element={<ProductUpdate />} />
       </Route>
-
-
-
     </Route>
   )
-)
+);
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   // <StrictMode>
   //   <App />
   // </StrictMode>,
   <Provider store={store}>
-    <RouterProvider router={router} />
+    <PayPalScriptProvider>
+      <RouterProvider router={router} />
+    </PayPalScriptProvider>
   </Provider>
-  
 );
